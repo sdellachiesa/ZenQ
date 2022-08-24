fe96af6715b5f55e3f2d229e1f3289462ead55cd
stefano della chiesa <stefano.dellachiesa@gmail.com>
2022-08-24 14:14:28 +0200

    Initial script and test query Zenodo API

---
+++ ZenodoAPIQuery.R
@@ -0,0 +1,14 @@
+
+## OAI Zenodo query
+#install.packages("oai")
+library(knitr)
+library(tidyverse)
+library(httr)
+library(oai)
+library(xml2)
+opts_chunk$set(echo=T)
+
+# Query Leibniz IOER Community data
+record_list<- list_records("https://zenodo.org/oai2d",metadataPrefix="oai_datacite",set="user-ioer_dresden")
+
+kable(record_list %>% select(identifier.3,title))
---
+++ twest.R
@@ -0,0 +1,6 @@
+
+
+if(!require(zen4R)) install.packages(zen4R)
+library(zen4R)
+
+download_zenodo("10.5281/zenodo.6322169")
